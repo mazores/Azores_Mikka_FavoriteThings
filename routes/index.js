81c26b5a259f2c2294cdf340cc1d653c42809c37
mazores <m_azores91446@fanshaweonline.ca>
2019-10-31 12:55:56 -0400

    adding lightbox and creating array

--- routes/index.js
+++ routes/index.js
@@ -12,11 +12,42 @@ router.get('/', (req, res) => {
     sql.query(query, (err, result) => {
         if (err) { throw err; console.log(err); }
 
-        console.log(result); // should see objects wrapped in an array
+        //console.log(result); // should see objects wrapped in an array
 
         // render the home view with dynamic data
         res.render('home', { people: result }); // data is all the details on the db, you can call it whatever you want
         //can be team, can be people, can be data; result can't be changed
+    })
+})
+
+router.get('/:id', (req, res) => {
+    // should really get the user data here and then fetch it thru, but let's try this asynchronously
+    console.log('at the user route');
+    console.log(req.params.id); // 1, 2, 3 or whatever comes after the slash
+
+    let query = `SELECT * FROM tbl_bio WHERE profID="${req.params.id}"`;
+
+    sql.query(query, (err, result) => {
+        if (err) { throw err; console.log(err); }
+
+        console.log(result); // should see objects wrapped in an array
+
+        // convert social property into an array
+        // before we send it through
+        // map is an array method that lets you map one value to another (convert it)
+        result[0].social = result[0].social.split(",").map(function(item) {
+            item = item.trim();
+            // item.trim() removes any empty white space from text
+
+            return item;
+        })
+
+        console.log("after trim / converion:", result[0]);
+
+        // render the home view with dynamic data
+        //res.render('home', { people: result }); // data is all the details on the db, you can call it whatever you want
+        //can be team, can be people, can be data; result can't be changed
+        res.json(result[0]);
     })
 })
 
